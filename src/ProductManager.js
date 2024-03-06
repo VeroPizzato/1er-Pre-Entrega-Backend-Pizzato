e57bd770d90770a909723dee592ad7adb9152e3c
VeroPizzato <verizzato@gmail.com>
2024-03-06 00:30:10 -0300

    subo cambios

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -60,46 +60,56 @@ class ProductManager {
         return id
     }
 
-    #soloNumYletras = (code) => {
+    soloNumYletras = (code) => {
         return (/^[a-z A-Z 0-9]+$/.test(code))
     }
 
-    addProduct = async (title, description, price, thumbnail, code, stock) => {
-        if (title.trim().length === 0) {
-            console.error("Error. El campo titulo es invalido.")
-            return
-        }
+    addProduct = async (title, description, price, thumbnail, code, stock, status, category) => {
+        // if (title.trim().length === 0) {
+        //     console.error("Error. El campo titulo es invalido.")
+        //     return
+        // }
 
-        if (description.trim().length === 0) {
-            console.error("Error. El campo descripción es invalido.")
-            return
-        }
+        // if (description.trim().length === 0) {
+        //     console.error("Error. El campo descripción es invalido.")
+        //     return
+        // }
 
-        if (isNaN(price)) {
-            console.error("Error. El campo precio es invalido.")
-            return
-        }
+        // if (isNaN(price)) {
+        //     console.error("Error. El campo precio es invalido.")
+        //     return
+        // }
 
-        if (thumbnail.trim().length === 0) {
-            console.error("Error. El campo ruta de imagen es invalido.")
-            return
-        }
+        // if (thumbnail.trim().length === 0) {
+        //     console.error("Error. El campo ruta de imagen es invalido.")
+        //     return
+        // }
 
-        if (isNaN(stock)) {
-            console.error("Error. El campo stock es invalido.")
-            return
-        }
+        // if (isNaN(stock)) {
+        //     console.error("Error. El campo stock es invalido.")
+        //     return
+        // }
 
-        if (!this.#soloNumYletras(code)) {
-            console.error("Error. El campo codigo identificador es invalido.")
-            return
-        }
+        // if (!this.#soloNumYletras(code)) {
+        //     console.error("Error. El campo codigo identificador es invalido.")
+        //     return
+        // }
 
-        const codeIndex = this.#products.findIndex(e => e.code === code)
-        if (codeIndex !== -1) {
-            console.error("Codigo ya existente")
-            return
-        }
+        // const codeIndex = this.#products.findIndex(e => e.code === code)
+        // if (codeIndex !== -1) {
+        //     console.error("Codigo ya existente")
+        //     return
+        // }
+
+        // if (status != true && status != false) {
+        //     console.error("Error. El campo categoría es invalido.")
+        //     return
+        // }
+
+        // if (category.trim().length === 0) {
+        //     console.error("Error. El campo categoría es invalido.")
+        //     return
+        // }
 
         const product = {
             id: this.#getNuevoId(),
@@ -108,7 +118,9 @@ class ProductManager {
             price,
             thumbnail,
             code,
-            stock
+            stock,
+            status,
+            category
         }
 
         this.#products.push(product)
@@ -121,50 +133,76 @@ class ProductManager {
         await fs.promises.writeFile(this.path, fileProducts)
     }
 
-    updateProduct = async (id, newTitle, newDescription, newPrice, newThumbnail, newCode, newStock) => {
+    updateProduct = async (id, producto) => {
+        if (producto.title !== '') {
+            this.#products[id].title = producto.title;
+        }
+        if (producto.description !== '') {
+            this.#products[id].description = producto.description;
+        }
+        this.#products[id].price = producto.price;
 
-        const codeIndex = this.#products.findIndex(e => e.id === id);
-        if (codeIndex === -1) {
-            console.error("Producto con ID:" + id + " not Found");
+        if (producto.thumbnail !== '') {
+            this.#products[id].thumbnail = producto.thumbnail;
         }
-        else {
-            if (newTitle !== '') {
-                this.#products[codeIndex].title = newTitle;
-            }            
-            if (newDescription !== '') {
-                this.#products[codeIndex].description = newDescription;
-            }           
-            if (newPrice !== '') {
-                if (isNaN(newPrice)) {
-                    console.error("Error. El campo precio es invalido.")
-                    return
-                }
-                this.#products[codeIndex].price = newPrice;
-            }
-            if (newThumbnail !== '') {
-                this.#products[codeIndex].thumbnail = newThumbnail;
-            }            
-            if (newCode !== '') {
-                const existingCode = listadoProductos.findIndex(item => item.code === newCode);
-                if (existingCode !== -1) {
-                    console.error("Codigo Ya existente");
-                    return;
-                }
-                if (!this.#soloNumYletras(newCode)) {
-                    console.error("Error. El campo codigo identificador es invalido.")
-                    return
-                }
-                this.#products[codeIndex].code = newCode;
-            }
-            if (newStock !== '') {
-                if (isNaN(newStock)) {
-                    console.error("Error. El campo stock es invalido.")
-                    return
-                }
-                this.#products[codeIndex].stock = newStock;
-            }
-            await this.#updateProducts()
+        this.#products[id].code = producto.code;
+        this.#products[id].stock = producto.stock;
+        if (producto.status !== '') {
+            this.#products[id].status = producto.status;
         }
+        if (producto.category !== '') {
+            this.#products[id].category = producto.category;
+        }
+        await this.#updateProducts()
+        // updateProduct = async (id, newTitle, newDescription, newPrice, newThumbnail, newCode, newStock, newStatus, newCategory) => {
+        // const codeIndex = this.#products.findIndex(e => e.id === id);
+        // if (codeIndex === -1) {
+        //     console.error("Producto con ID:" + id + " not Found");
+        // }
+        // else {
+        //     if (newTitle !== '') {
+        //         this.#products[codeIndex].title = newTitle;
+        //     }            
+        //     if (newDescription !== '') {
+        //         this.#products[codeIndex].description = newDescription;
+        //     }           
+        //     if (newPrice !== '') {
+        //         if (isNaN(newPrice)) {
+        //             console.error("Error. El campo precio es invalido.")
+        //             return
+        //         }
+        //         this.#products[codeIndex].price = newPrice;
+        //     }
+        //     if (newThumbnail !== '') {
+        //         this.#products[codeIndex].thumbnail = newThumbnail;
+        //     }            
+        //     if (newCode !== '') {
+        //         const existingCode = listadoProductos.findIndex(item => item.code === newCode);
+        //         if (existingCode !== -1) {
+        //             console.error("Codigo Ya existente");
+        //             return;
+        //         }
+        //         if (!this.soloNumYletras(newCode)) {
+        //             console.error("Error. El campo codigo identificador es invalido.")
+        //             return
+        //         }
+        //         this.#products[codeIndex].code = newCode;
+        //     }
+        //     if (newStock !== '') {
+        //         if (isNaN(newStock)) {
+        //             console.error("Error. El campo stock es invalido.")
+        //             return
+        //         }
+        //         this.#products[codeIndex].stock = newStock;
+        //     }
+        //     if (newStatus !== '') {
+        //         this.#products[codeIndex].status = newStatus;
+        //     }
+        //     if (newCategory !== '') {
+        //         this.#products[codeIndex].category = newCategory;
+        //     }
+        //     await this.#updateProducts()
+        // }        
     }
 
     deleteProduct = async (idProd) => {
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,44 +1,100 @@
-const { Router } = require('express');
-const router = Router();
+const { Router } = require('express')
+const router = Router()
 
-const ProductManager = require('../ProductManager');
+const ProductManager = require('../ProductManager')
 
 const filename = `${__dirname}/../../productos.json`
 const productsManager = new ProductManager(filename)
 
 // Middleware para validacion de datos al agregar un producto (post)
-function validarDatos (req, res, next) {
-   
-
-
-
+function validarNuevoProducto(req, res, next) {
+    const { titulo, descripcion, precio, ruta, codigo, stock, status, categoria } = req.body;
+    if (!titulo || !descripcion || !precio || !codigo || !stock || !status || !categoria) {
+        return res.status(400).json({ error: 'Todos los campos son obligatorios, salvo la ruta de la imagen' });
+    }
+    if (isNaN(precio) || isNaN(stock)) {
+        res.status(400).json({ error: "Invalid number format" })
+        return
+    }
+    const codeIndex = productsManager.getProducts.findIndex(e => e.code === codigo)
+    if (codeIndex !== -1) {
+        res.status(400).json({ error: "Codigo ya existente" })
+        return
+    }
+    if (!productsManager.soloNumYletras(codigo)) {
+        res.status(400).json({ error: "El campo codigo identificador es invalido." })
+        return
+    }
+    if (status != true && status != false) {
+        res.status(400).json({ error: "El campo status es invalido." })
+        return
+    }
     next()
 }
 
+// Middleware para validacion de datos al actualizar un producto (put)
+// Si algun dato es vacio no se actualiza
+function validarProdActualizado(req, res, next) {
+    const { newTitle, newDescription, newPrice, newThumbnail, newCode, newStock, newStatus, newCategory } = req.body;
+    const { prodId } = req.params.id
+    const codeIndex = productsManager.getProducts.findIndex(e => e.id === prodId);
+    if (codeIndex === -1) {
+        res.status(400).json({ error: "Producto con ID:" + prodId + " not Found" })
+        return
+    }
+    else {
+        if (newPrice !== '') {
+            if (isNaN(newPrice)) {
+                res.status(400).json({ error: "Error. El campo precio es invalido." })
+                return
+            }
+        }
+        if (newStock !== '') {
+            if (isNaN(newStock)) {
+                res.status(400).json({ error: "El campo stock es invalido." })
+                return
+            }
+        }
+        if (newCode !== '') {
+            const existingCode = listadoProductos.findIndex(item => item.code === newCode);
+            if (existingCode !== -1) {
+                res.status(400).json({ error: "Codigo ya existente" })
+                return
+            }
+            if (!productsManager.soloNumYletras(newCode)) {
+                res.status(400).json({ error: "El campo codigo identificador es invalido." })
+                return
+            }
+        }
+        if (newStatus != true && newStatus != false) {
+            res.status(400).json({ error: "El campo status es invalido." })
+            return
+        }
+    }
+    next()
+}
 
 router.get('/', async (req, res) => {
     let cantLimite = req.query.limit
-    
+
     const listadoProductos = await productsManager.getProducts()
     let prodFiltrados = []
 
-    if (cantLimite){
-        if (isNaN(cantLimite) || (cantLimite<0) ) {
+    if (cantLimite) {
+        if (isNaN(cantLimite) || (cantLimite < 0)) {
             // HTTP 400 => hay un error en el request o alguno de sus parámetros
-            res.status(400).json({ error: "Invalid ID format" })
+            res.status(400).json({ error: "Invalid limit format" })
             return
         }
-        prodFiltrados = listadoProductos.splice(0, cantLimite)
+        prodFiltrados = listadoProductos.slice(0, cantLimite)
     }
     else prodFiltrados = listadoProductos
 
     res.status(200).json(prodFiltrados)  // HTTP 200 OK
-
-});
+})
 
 router.get('/:pid', (req, res) => {
-
-    const productId = Number.parseInt(req.params.pid)
+    const productId = +req.params.pid
 
     if (isNaN(productId)) {
         // HTTP 400 => hay un error en el request o alguno de sus parámetros
@@ -46,21 +102,39 @@ router.get('/:pid', (req, res) => {
         return
     }
 
-    const productByID = productsManager.getProductById(productId);    
+    const productByID = productsManager.getProductById(productId)
     if (!productByID) {
-        res.status(404).json({ Error: "Id inexistente!"})  // HTTP 404 => el ID es válido, pero no se encontró ese producto
-        return;
+        res.status(404).json({ error: "Id inexistente!" })  // HTTP 404 => el ID es válido, pero no se encontró ese producto
+        return
     }
-    res.status(200).json(productByID);    // HTTP 200 OK
+    res.status(200).json(productByID)    // HTTP 200 OK
+})
 
+router.post('/', validarNuevoProducto, async (req, res) => {
+    const producto = req.body
+
+    const nuevoProducto = await productsManager.addProduct(producto.title, producto.description, producto.price, producto.thumbnail, producto.code, producto.stock, producto.status, producto.category)
+
+    res.status(201).json({ message: "Producto agregado correctamente", producto: nuevoProducto })
+})
+
+router.put('/:pid', validarProdActualizado, async (req, res) => {
+    const productID = +req.params.pid
+    const producto = req.body
+
+    await manejadorDeProductos.updateProduct(productID, producto);
+
+    return res.status(200).json({ message: "Producto actualizado correctamente" });
 });
 
-router.post('/', validarDatos, async (req, res) => {
-    const product = req.body
-    
-
-
-    
+router.delete('/:pid', async (req, res) => {
+    let idProd = +req.params.pid;
+    let DelproductByID = await manejadorDeProductos.deleteProduct(idProd);
+    if (!DelproductByID) {
+        res.status(404).json({ error: "Id inexistente!" })  // HTTP 404 => el ID es válido, pero no se encontró ese producto
+        return
+    }
+    res.status(200).json({ message: "Producto Eliminado correctamente" })    // HTTP 200 OK
 });
 
 const main = async () => {
@@ -68,4 +142,4 @@ const main = async () => {
 }
 main()
 
-module.exports = router;
+module.exports = router
