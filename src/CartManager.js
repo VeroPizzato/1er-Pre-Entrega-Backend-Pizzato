2492b302158892d59c647b7c6600a3df51e12ea3
VeroPizzato <verizzato@gmail.com>
2024-03-06 21:41:12 -0300

    subo cambios

--- src/CartManager.js
+++ src/CartManager.js
@@ -1,7 +1,77 @@
 const fs = require('fs')
 
 class CartManager {
+   
+    static #ultimoIdCart = 1
 
+    constructor(pathname) {
+        this.#carts = []
+        this.path = pathname
+    }
+
+    async #readCarts() {
+        try {
+            const fileCarts = await fs.promises.readFile(this.path, 'utf-8')
+            this.#carts = JSON.parse(fileCarts)
+        }
+        catch (err) {
+            return []
+        }
+    }
+
+    inicialize = async () => {
+        this.#carts = await this.getCarts()
+        CartManager.#ultimoIdCart = this.#getNuevoIdInicio()
+    }
+
+    #getNuevoIdInicio = () => {
+        let mayorID = 1
+        this.#carts.forEach(item => {
+            if (mayorID <= item.id)
+                mayorID = item.id
+        });
+        mayorID = mayorID + 1   
+        return mayorID
+    }
+
+    getCarts = async () => {
+        try {
+            await this.#readCarts()
+            return this.#carts
+        }
+        catch (err) {
+            return []
+        }
+    }   
+
+    #getNuevoId() {
+        const id = CartManager.#ultimoIdCart
+        CartManager.#ultimoIdCart++
+        return id
+    }
+
+    addCart = async (arrayCart) => {
+
+
+
+    }
+
+    getCartByCId = async (cid) => {
+        const codeIndex = this.#carts.findIndex(e => e.cid === cid)
+        if (codeIndex === -1) {
+            console.error(`Carrito con ID: ${cid} Not Found`)
+            return
+        } else {
+            return this.#carts[codeIndex]
+        }
+    }
+    
+    addProductToCart = async (cid, pid, quantity) => {
+
+
+
+
+    }
 
     
 }
--- src/routes/carts.js
+++ src/routes/carts.js
@@ -1,7 +1,38 @@
 const { Router } = require('express');
 const router = Router();
 
+const CartManager = require('../CartManager');
+
+const filename = `${__dirname}/../../carrito.json`
+const carritoManager = new CartManager(filename)
+
+router.post('/', async (req, res) => {
 
 
+    
+    
+
+})
+
+router.get('/:cid', async (req, res) => {
+    let cidCart = +req.params.cid;
+    let cartByCID = await carritoManager.getCartByCId(cidCart); 
+
+
+
+
+})
+
+router.post('/:cid/product/:pid', async (req, res) => {
+
+
+
+
+})
+
+const main = async () => {
+    await carritoManager.inicialize()
+}
+main()
 
 module.exports = router;
